1b1a9083f3f9bc20715bc0c7ff5de1359fc4382e
Hrvoje <stjepan.hrvoje@gmail.com>
2022-10-21 15:55:14 +0200

    Required puzzle solved

--- contracts/optimized_contracts/OptimizedRequire.sol
+++ contracts/optimized_contracts/OptimizedRequire.sol
@@ -4,13 +4,15 @@ pragma solidity 0.8.15;
 error CannotPurchase(); 
 
 contract OptimizedRequire {
-    uint256 lastPurchaseTime;
+    uint256 constant COOLDOWN = 1 minutes;
+    uint256 lastPurchaseTime = 1;
 
     function purchaseToken() external payable {
-        if (msg.value != 0.1 ether ||
-            block.timestamp < lastPurchaseTime + 1 minutes) {
-            revert CannotPurchase();
-        }
+        require(
+            msg.value == 0.1 ether &&
+                block.timestamp > lastPurchaseTime + COOLDOWN,
+            "cannot purchase"
+        );
         lastPurchaseTime = block.timestamp;
         // mint the user a token
     }
